90e4303c4bbfb0196a8cfbe74dfe6d7431040d63
presuit <chldudgh8256@naver.com>
2017-12-05 10:59:20 +0900

    Hello world

---
+++ Init.c
@@ -0,0 +1,5 @@
+#include "Init.h"
+
+void Init(){
+
+}
---
+++ Scheduler.c
@@ -0,0 +1,14 @@
+#include "Init.h"
+#include "Thread.h"
+#include "Scheduler.h"
+
+
+int		RunScheduler( void )
+{
+}
+
+
+void		_ContextSwitch(int curpid, int tpid)
+{
+}
+
